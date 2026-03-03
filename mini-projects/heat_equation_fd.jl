1b920e17889d4392f2ee7d01adb6eaf391c68981
Utku Yılmaz <utkyilmz@gmail.com>
2026-03-03 13:50:55 +0300

    Refactor heat equation solution into a function
    Refactored heat equation solver into a function and adjusted parameters for time step and iterations.

--- mini-projects/heat_equation_fd.jl
+++ mini-projects/heat_equation_fd.jl
@@ -1,45 +1,40 @@
-# heat_equation_fd.jl
-# Finite Difference solution of the 1D Heat Equation
-#
-# u_t = α u_xx
-
 using Plots
 
-# Parameters
-α = 1.0               # diffusion coefficient
-L = 1.0               # length of rod
-Nx = 50               # spatial points
-dx = L / (Nx - 1)
+function solve_heat_equation()
+    
+    α = 1.0
+    L = 1.0
+    Nx = 50
+    dx = L / (Nx - 1)
 
-dt = 0.0005           # time step
-Nt = 400              # number of time steps
+    dt = 0.0001
+    Nt = 2000
 
-# Spatial grid
-x = range(0, L, length=Nx)
+    x = range(0, L, length=Nx)
 
-# Initial condition: sine wave
-u = sin.(π .* x)
+    u = sin.(π .* x)
 
-# Boundary conditions
-u[1] = 0.0
-u[end] = 0.0
 
-# Time evolution
-for n in 1:Nt
-    u_new = copy(u)
+    u[1] = 0.0
+    u[end] = 0.0
 
-    for i in 2:Nx-1
-        u_new[i] = u[i] +
-            α * dt/dx^2 *
-            (u[i+1] - 2u[i] + u[i-1])
+    for n in 1:Nt
+        u_new = copy(u)
+
+        for i in 2:Nx-1
+            u_new[i] = u[i] + α * dt/dx^2 * (u[i+1] - 2u[i] + u[i-1])
+        end
+
+        u = u_new 
     end
 
-    u = u_new
+    
+    plot(x, u,
+         title="1D Heat Equation (Finite Difference)",
+         xlabel="x (Position)",
+         ylabel="Temperature",
+         legend=false,
+         color=:red, lw=2)
 end
 
-# Plot result
-plot(x, u,
-     title="Heat Equation Solution (Finite Difference)",
-     xlabel="x",
-     ylabel="Temperature",
-     legend=false)
+solve_heat_equation()
